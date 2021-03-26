b7d6f03153a6393a0c4bb40a16ebe3bc84f66ac3
Proladon <proladon@gmail.com>
2021-03-26 18:57:46 +0800

    vite init / import all img

---
+++ src/components/WaterFlow.vue
@@ -0,0 +1,29 @@
+<template>
+  This is Water Flow
+  <!-- {{img.source}} -->
+  <img v-for="img in img.source" :key="img" :src="img" alt="">
+  <!-- <img :src="a" alt=""> -->
+</template>
+
+<script setup>
+import { onMounted, reactive } from "vue";
+
+// all img file path
+const img = reactive({
+    source:[]
+})
+
+onMounted(()=>{
+    
+    // get all img file path & store in img.source
+    for(let num=1; num<13; num++){
+        let filePath = `/src/assets/img (${num}).jpg`
+        img.source.push(filePath)
+    }
+
+})
+
+</script>
+
+<style>
+</style>
